27293fbfda7011fa9f4b7b43367ca0ee03d6bbb1
Dheeraj Sawlani <dheerajsawlani101@gmail.com>
2025-12-11 17:32:48 +0530

    Playing with routes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
-
 app.use("/test",(req,res)=>{
     res.send("Test from server");
 });
-app.use((req,res)=>{
-    res.send("Hello from server");
+app.get("/user/:userid",(req,res)=>{
+    console.log(req.params);
+    res.send({firstname:"Dheeraj",lastname:"Sawlani"});
 });
-
+app.post("/user",(req,res)=>{
+    res.send("Data saved.");
+});
+// app.use("/",(req,res)=>{
+//     res.send("Hello from server");
+// });
+// this order is important, because whatever matches first gets executed
 app.listen(3000,()=>{
     console.log('Server started successfully.');
 });
